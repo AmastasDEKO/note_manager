68f64a7f6273d9858bc2f778294266b06b9f7569
AmastasDEKO <makl.amastas22@gmail.com>
2024-12-25 16:37:38 +0500

    homework(Grade1:Stage1:Task5)

--- final.py
+++ final.py
@@ -2,26 +2,26 @@
 note = {}
 
 # Запрос данных о заметке у пользователя
-note['Имя пользователя'] = input("Введите имя пользователя: ")
+note['username'] = input("Введите имя пользователя: ")
 
 # Ввод заголовков
-note['Заголовки'] = []
+note['titles'] = []
 for i in range(3):
     title = input(f"Введите название заголовка {i+1}: ")
-    note['Заголовки'].append(title.capitalize())
+    note['titles'].append(title.capitalize())
 
 # Продолжение ввода данных о заметке
-note['Описание'] = input("Введите описание заметки: ")
-note['Статус'] = input("Введите статус заметки (Активна/Выполнена): ")
-note['Дата создания'] = input("Введите дату создания заметки (день.месяц.год): ").split(sep='.', maxsplit=-1)
-note['Дата истечения'] = input("Введите дату истечения заметки (день.месяц.год): ").split(sep='.', maxsplit=-1)
+note['content'] = input("Введите описание заметки: ")
+note['status'] = input("Введите статус заметки (Активна/Выполнена): ")
+note['created_date'] = input("Введите дату создания заметки (день.месяц.год): ").split(sep='.', maxsplit=-1)
+note['issue_date'] = input("Введите дату истечения заметки (день.месяц.год): ").split(sep='.', maxsplit=-1)
 
 # Вывод введенных данных
 print("\n   Новая заметка!")
 for key, value in note.items():
-    if key == "Заголовки":
+    if key == "titles":
         print(f"{key.capitalize()}:", ", ".join(value))
-    elif key == "Дата создания" or key == "Дата истечения":
+    elif key == "created_date" or key == "issue_date":
         print(f"{key.capitalize()}:", '.'.join(value[:2]) )
     else:
         print(f"{key.capitalize()}: {value.capitalize()}")
