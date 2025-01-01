2fcb3b0907ccf796da9a77b2b4e610b4464d4e10
AmastasDEKO <makl.amastas22@gmail.com>
2025-01-02 01:12:27 +0500

    homework(Grade1:Stage2:Task2)

--- update_status.py
+++ update_status.py
@@ -13,25 +13,32 @@ print("Текущий статус заметки:", note["status"])
 # Цикл изменения статуса
 while True:
     # Запрос на изменение статуса и проверка корректности ввода
-    changed = input("Хотите изменить статус заметки? (Да/Нет)\nВвод: ")
-    if changed == "Да" or changed == "да":
+    changed = input("Хотите изменить статус заметки? (Да/Нет)\nВвод: ").strip().capitalize()
+    if changed == "Да":
        #Цикл ввода статуса
        while True:
            # Ввод нового статуса и проверка корректности ввода
-           new_status = input("Введите статус цифрой:\n1. Выполнено\n2. В процессе\n3. Отложено\nВвод: ")
-
+           new_status = input("Введите статус:\n1. Выполнено\n2. В процессе\n3. Отложено\nВвод: ").strip().capitalize()
+           #Проверка по ключу(цифре)
            if new_status in choice_status.keys():
                # Изменение и вывод нового статуса
                note["status"] = choice_status[new_status]
                print(f"Статус успешно обновлен на: {note["status"]}")
                #Выход из второго цикла
                break
+           # Проверка по значению(словам)
+           elif new_status in choice_status.values():
+               # Изменение и вывод нового статуса
+               note["status"] = new_status
+               print(f"Статус успешно обновлен на: {note["status"]}")
+               # Выход из второго цикла
+               break
            else:
                print("Ошибка ввода (Допустимо: 1, 2, 3)")
        # Выход из первого цикла
        break
 
-    elif changed == "Нет" or changed == "нет":
+    elif changed == "Нет":
         print("Статус не изменен")
         break
     else:
