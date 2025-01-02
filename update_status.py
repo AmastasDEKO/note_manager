48a5295a8a7c7b050f3737b1ff08b7f37702db99
AmastasDEKO <makl.amastas22@gmail.com>
2025-01-02 05:46:42 +0500

    homework(Grade1:Stage2:Task2)

--- update_status.py
+++ update_status.py
@@ -7,39 +7,45 @@ choice_status = {"1":"Выполнено",
                  "3":"Отложено"
                  }
 
+# Функция ввода нового статуса
+def changed_status():
+        # Ввод нового статуса и проверка корректности ввода
+        new_status = input("Введите статус:\n1. Выполнено\n2. В процессе\n3. Отложено\nВвод: ").strip().capitalize()
+        # Проверка по ключу(цифре)
+        if new_status in choice_status.keys():
+            # Изменение и вывод нового статуса
+            note["status"] = choice_status[new_status]
+            print(f"Статус успешно обновлен на: {note["status"]}")
+            # Выход из функции
+            return
+        # Проверка по значению(словам)
+        elif new_status in choice_status.values():
+            # Изменение и вывод нового статуса
+            note["status"] = new_status
+            print(f"Статус успешно обновлен на: {note["status"]}")
+            # Выход из второго цикла
+            return
+        else:
+            print("Ошибка ввода (Допустимо: 1, 2, 3)")
+            return changed_status()
+
+
+# Функция запроса на ввод нового статуса
+def can_changed():
+        changed = input("Хотите изменить статус заметки? (Да/Нет)\nВвод: ").strip().capitalize()
+        if changed == "Да":
+            # Вызов функции ввода нового статуса
+            changed_status()
+            return
+        elif changed == "Нет":
+            print("Статус не изменен")
+            return
+        else:
+            print("Ошибка ввода (Допустимо: Да, Нет)")
+            return can_changed()
+
+
 # Вывод текущего статуса
 print("Текущий статус заметки:", note["status"])
-
-# Цикл изменения статуса
-while True:
-    # Запрос на изменение статуса и проверка корректности ввода
-    changed = input("Хотите изменить статус заметки? (Да/Нет)\nВвод: ").strip().capitalize()
-    if changed == "Да":
-       #Цикл ввода статуса
-       while True:
-           # Ввод нового статуса и проверка корректности ввода
-           new_status = input("Введите статус:\n1. Выполнено\n2. В процессе\n3. Отложено\nВвод: ").strip().capitalize()
-           #Проверка по ключу(цифре)
-           if new_status in choice_status.keys():
-               # Изменение и вывод нового статуса
-               note["status"] = choice_status[new_status]
-               print(f"Статус успешно обновлен на: {note["status"]}")
-               #Выход из второго цикла
-               break
-           # Проверка по значению(словам)
-           elif new_status in choice_status.values():
-               # Изменение и вывод нового статуса
-               note["status"] = new_status
-               print(f"Статус успешно обновлен на: {note["status"]}")
-               # Выход из второго цикла
-               break
-           else:
-               print("Ошибка ввода (Допустимо: 1, 2, 3)")
-       # Выход из первого цикла
-       break
-
-    elif changed == "Нет":
-        print("Статус не изменен")
-        break
-    else:
-        print("Ошибка ввода (Допустимо: Да, Нет)")
+# Вызов функции на изменение статуса
+can_changed()
