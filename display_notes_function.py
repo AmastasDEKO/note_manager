20a58ce68639747246ac25a03a5edb1b8945f6b5
AmastasDEKO <makl.amastas22@gmail.com>
2025-01-10 07:54:06 +0500

    Grade1_Stage3_Task3_Маключенко_Анастасия

--- display_notes_function.py
+++ display_notes_function.py
@@ -8,31 +8,37 @@
 
 # Подключение библиотек datetime, prettytable, colorama
 from datetime import datetime
+
 from prettytable import PrettyTable
-from colorama import init, Fore, Back, Style
+from colorama import init, Fore, Style
 
 # Определение автоматического сбрасывания настроек цвета текста
 init(autoreset=True)
 
 # Функция сортировки выборкой
 def sort_by_date(_notes, name_date):
+    sort_notes = _notes[:]
     # Перебор заметок в списке
-    for i in range(len(_notes)):
+    for i in range(len(sort_notes)):
         # Перемещаем i-ую заметку, дату и индекс в переменные
-        note = _notes[i]
+        note = sort_notes[i]
         nearest_date = note[name_date]
         index_nearest_date = i
         # Перебор заметок в списке начиная с i+1
-        for j in range(i+1, len(_notes)):
+        for j in range(i+1, len(sort_notes)):
             # Перемещаем j-ую заметку в переменную
-            next_note = _notes[j]
+            next_note = sort_notes[j]
             # Сравниваем даты
             if next_note[name_date] < nearest_date:
+
                 # Меняем наименьшую дату и индекс
                 nearest_date = next_note[name_date]
                 index_nearest_date = j
         # Меняем i-тую заметку с наименьшей местами
-        _notes[i], _notes[index_nearest_date] = _notes[index_nearest_date],_notes[i]
+        else:
+            sort_notes[i], sort_notes[index_nearest_date] = sort_notes[index_nearest_date],sort_notes[i]
+    else:
+        return sort_notes
 
 # Функция для вывода списка заметок
 def display_notes(_notes):
@@ -67,10 +73,29 @@ def display_notes(_notes):
                                "\nВвод: ").strip().lower()
         # Неполный вывод
         if filter_display in filter_choice[:2]:
+            # Определяем страницу
+            page = 1
             print("Список заметок:")
-            print("---------------")
             # Цикл вывода заметок из списка
             for i in range(len(_notes)):
+                # Определяем конечный элемент страницы
+                index = page * 5
+                # Сравниваем текущий элемент списка и конечный элемент страницы
+                if i >= index:
+                    print("---------------")
+                    print(f"Страница №{page}")
+                    # Запрос на открытие следующей страницы
+                    while True:
+                        input_open_next_page = input("Желаете открыть следующую страницу? "
+                                                     "(Да/Нет)\nВвод:").strip().lower()
+                        if input_open_next_page == "да":
+                            page += 1
+                            break
+                        elif input_open_next_page == "нет":
+                            return
+                        else:
+                            print(Fore.RED + "Ошибка ввода (Допустимо: Да, Нет)")
+                print("---------------")
                 # Помещаем текущую заметку в переменную
                 note = _notes[i]
                 # Вывод данных заметки
@@ -79,20 +104,39 @@ def display_notes(_notes):
                     if key == "titles":
                             # Меняем цвет заголовков на яркий жёлтый
                         print(f"{note_print[key]}: {Fore.YELLOW+Style.BRIGHT+", ".join(value).title()}")
-                else:
-                    print("---------------")
             else:
+                print("---------------")
+                print(f"Страница №{page}")
                 break
         # Сортировка по дате создания
         elif filter_display in filter_choice[2:4]:
             # Вызываем функцию сортировки
-            sort_by_date(_notes, "created_date")
+            sort_notes = sort_by_date(_notes, "created_date")
+            # Определяем страницу
+            page = 1
             print("Список заметок:")
-            print("---------------")
             # Цикл вывода заметок из списка
-            for i in range(len(_notes)):
+            for i in range(len(sort_notes)):
+                # Определяем конечный элемент страницы
+                index = page * 5
+                # Сравниваем текущий элемент списка и конечный элемент страницы
+                if i >= index:
+                    print("---------------")
+                    print(f"Страница №{page}")
+                    # Запрос на открытие следующей страницы
+                    while True:
+                        input_open_next_page = input("Желаете открыть следующую страницу? "
+                                                     "(Да/Нет)\nВвод:").strip().lower()
+                        if input_open_next_page == "да":
+                            page += 1
+                            break
+                        elif input_open_next_page == "нет":
+                            return
+                        else:
+                            print(Fore.RED + "Ошибка ввода (Допустимо: Да, Нет)")
+                print("---------------")
                 # Помещаем текущую заметку в переменную
-                note = _notes[i]
+                note = sort_notes[i]
                 # Вывод данных заметки
                 print(f"Заметка №{i + 1}")
                 for key, value in note.items():
@@ -108,20 +152,39 @@ def display_notes(_notes):
                             print(f"{note_print[key]}: {'.'.join(splitting[:0:-1])}")
                     else:
                         print(f"{note_print[key]}: {value.capitalize()}")
-                else:
-                    print("---------------")
             else:
+                print("---------------")
+                print(f"Страница №{page}")
                 break
         # Сортировка по дате дедлайна
         elif filter_display in filter_choice[4:6]:
             # Вызываем функцию сортировки
-            sort_by_date(_notes, "issue_date")
+            sort_notes = sort_by_date(_notes, "issue_date")
+            # Определяем страницу
+            page = 1
             print("Список заметок:")
-            print("---------------")
             # Цикл вывода заметок из списка
-            for i in range(len(_notes)):
+            for i in range(len(sort_notes)):
+                # Определяем конечный элемент страницы
+                index = page * 5
+                # Сравниваем текущий элемент списка и конечный элемент страницы
+                if i >= index:
+                    print("---------------")
+                    print(f"Страница №{page}")
+                    # Запрос на открытие следующей страницы
+                    while True:
+                        input_open_next_page = input("Желаете открыть следующую страницу? "
+                                                     "(Да/Нет)\nВвод:").strip().lower()
+                        if input_open_next_page == "да":
+                            page += 1
+                            break
+                        elif input_open_next_page == "нет":
+                            return
+                        else:
+                            print(Fore.RED + "Ошибка ввода (Допустимо: Да, Нет)")
+                print("---------------")
                 # Помещаем текущую заметку в переменную
-                note = _notes[i]
+                note = sort_notes[i]
                 # Вывод данных заметки
                 print(f"Заметка №{i + 1}")
                 for key, value in note.items():
@@ -132,14 +195,14 @@ def display_notes(_notes):
                         splitting = splitting[0].split(sep="-", maxsplit=-1)
                         if key == "issue_date":
                             # Меняем цвет даты дедлайна на красный
-                            print(f"{note_print[key]}: {Fore.RED+'.'.join(splitting[:0:-1])}")
+                            print(f"{note_print[key]}: {Fore.RED + '.'.join(splitting[:0:-1])}")
                         else:
                             print(f"{note_print[key]}: {'.'.join(splitting[:0:-1])}")
                     else:
                         print(f"{note_print[key]}: {value.capitalize()}")
-                else:
-                    print("---------------")
             else:
+                print("---------------")
+                print(f"Страница №{page}")
                 break
         # Вывод в виде таблицы
         elif filter_display in filter_choice[6:8]:
@@ -169,14 +232,33 @@ def display_notes(_notes):
             break
         # Без фильтров
         elif filter_display in filter_choice[8:]:
+            # Определяем страницу
+            page = 1
             print("Список заметок:")
-            print("---------------")
             # Цикл вывода заметок из списка
             for i in range(len(_notes)):
+                # Определяем конечный элемент страницы
+                index = page * 5
+                # Сравниваем текущий элемент списка и конечный элемент страницы
+                if i >= index:
+                    print("---------------")
+                    print(f"Страница №{page}")
+                    # Запрос на открытие следующей страницы
+                    while True:
+                        input_open_next_page = input("Желаете открыть следующую страницу? "
+                                                     "(Да/Нет)\nВвод:").strip().lower()
+                        if input_open_next_page == "да":
+                            page += 1
+                            break
+                        elif input_open_next_page == "нет":
+                            return
+                        else:
+                            print(Fore.RED + "Ошибка ввода (Допустимо: Да, Нет)")
+                print("---------------")
                 # Помещаем текущую заметку в переменную
                 note = _notes[i]
                 # Вывод данных заметки
-                print(f"Заметка №{i+1}")
+                print(f"Заметка №{i + 1}")
                 for key, value in note.items():
                     if key == "titles":
                         print(f"{note_print[key]}: {", ".join(value).title()}")
@@ -186,15 +268,17 @@ def display_notes(_notes):
                         print(f"{note_print[key]}: {'.'.join(splitting[:0:-1])}")
                     else:
                         print(f"{note_print[key]}: {value.capitalize()}")
-                else:
-                    print("---------------")
-            else:
-                break
 
+            else:
+                print("---------------")
+                print(f"Страница №{page}")
+                break
+        # Обработка ошибок ввода
         else:
-            print("Ошибка ввода (Допустимо:1, 2, 3, 4, 5 или"
+            print(Fore.RED+"Ошибка ввода (Допустимо:1, 2, 3, 4, 5 или"
                   " Неполный вывод, Сортировка по дате создания,"
                   " Сортировка по дате дедлайна, Вывод в виде таблицы, Без фильтров")
+# Главная часть
 if __name__ == "__main__":
     # Создание списка с заметками
     notes1 = [{
@@ -205,15 +289,15 @@ if __name__ == "__main__":
         "created_date": datetime(2025, 1, 2),
         "issue_date": datetime(2025, 1, 10)
     },
-        {
+              {
             "username": "кристина",
             "titles": ["любовь", "подарок"],
             "content": "выбрать подарок на 14 февраля",
             "status": "новая",
-            "created_date": datetime.today(),
+            "created_date": datetime(2025, 1, 9),
             "issue_date": datetime(2025, 1, 15)
         },
-        {
+              {
             "username": "максим",
             "titles": ["стиральная машина", "дом"],
             "content": "отремонтировать стиральную машину в доме бабушки",
@@ -221,7 +305,7 @@ if __name__ == "__main__":
             "created_date": datetime(2025, 1, 10),
             "issue_date": datetime(2025, 1, 25)
         },
-        {
+              {
             "username": "анна",
             "titles": ["машина", "поход"],
             "content": "загрузить машину инвентарем для похода в горы",
@@ -229,13 +313,21 @@ if __name__ == "__main__":
             "created_date": datetime(2025, 1, 2),
             "issue_date": datetime(2025, 1, 20)
         },
-        {
+              {
             "username": "анна",
             "titles": ["рисование", "любовь"],
             "content": "написать сочинение об любви к рисованию",
             "status": "в процессе",
             "created_date": datetime(2025, 1, 6),
             "issue_date": datetime(2025, 1, 30)
+        },
+              {
+            "username": "вика",
+            "titles": ["работа", "деньги"],
+            "content": "найти новую работу с зарплатой в 100К",
+            "status": "в процессе",
+            "created_date": datetime(2025, 1, 13),
+            "issue_date": datetime(2025, 3, 20)
         }]
 
     # Создание пустого списка
