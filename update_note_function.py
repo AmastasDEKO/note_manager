d8b7e390d917b45046f475445fde2b2e5aa5e450
AmastasDEKO <makl.amastas22@gmail.com>
2025-01-15 19:42:47 +0500

    Grade1_Stage3_Task2_Маключенко_Анастасия

--- update_note_function.py
+++ update_note_function.py
@@ -139,13 +139,13 @@ def update_note(_note):
                     if new_status in choice_status.keys():
                         # Изменение и вывод нового статуса
                         _note["status"] = choice_status[new_status]
-                        print(Fore.GREEN+f"Статус успешно обновлен на: {_note["status"].capitalize()}")
+                        print(Fore.GREEN+f"Статус успешно обновлен на: {_note['status'].capitalize()}")
                         break
                     # Проверка по значению(словам)
                     elif new_status in choice_status.values():
                         # Изменение и вывод нового статуса
                         _note["status"] = new_status
-                        print(Fore.GREEN+f"Статус успешно обновлен на: {_note["status"].capitalize()}")
+                        print(Fore.GREEN+f"Статус успешно обновлен на: {_note['status'].capitalize()}")
                         break
                     else:
                         print(Fore.RED+"Ошибка ввода (Допустимо: 1, 2, 3 или Выполнено, В процессе, Отложено)")
