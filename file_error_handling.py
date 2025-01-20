873c02b5a38e53f9137e3da085f504ca8e8e74d6
AmastasDEKO <makl.amastas22@gmail.com>
2025-01-20 10:25:52 +0500

    Grade1_Stage4_Task3_Маключенко_Анастасия

--- file_error_handling.py
+++ file_error_handling.py
@@ -1,8 +1,4 @@
 # Подключение формата даты, цвет текста, json и функции вывода
-from datetime import datetime
-import json
-from colorama import init, Fore
-from display_notes_function import display_notes
 
 # Основная часть кода
 if __name__ == "__main__":
