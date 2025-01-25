f78a11e9536388891634f3a90748c56481e288f8
AmastasDEKO <makl.amastas22@gmail.com>
2025-01-25 08:22:34 +0500

    data directory (Stage 4 here!)

--- data/__init__.py
+++ data/__init__.py
@@ -3,3 +3,7 @@ from .append_notes_to_file import append_notes_to_file
 from .save_notes_json import save_notes_json
 from .load_notes_from_file import load_notes_from_file
 from .save_notes_json import save_notes_json
+from .update_note_function import update_note
+from .delete_note import delete_notes
+from .create_note_function import create_note
+from .search_notes_function import search_notes
